b1c2177295da15b7bff53472b9fc3abec470addd
Allan Im <allan@vogle.com>
2018-10-21 13:19:53 -0400

    Add card information delete request & response

---
+++ sbpayment-client/src/main/java/com/vogle/sbpayment/client/response/CardInfoDeleteResponse.java
@@ -0,0 +1,20 @@
+package com.vogle.sbpayment.client.response;
+
+import com.vogle.sbpayment.client.SpsResponse;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * Card information delete response
+ *
+ * @author Allan Im
+ **/
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@JacksonXmlRootElement(localName = "sps-api-response")
+public class CardInfoDeleteResponse extends CardInfoElements implements SpsResponse {
+}
