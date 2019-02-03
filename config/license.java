6d5a6a5bad801700c336f8aec26aabe479af6763
Allan Im <allan@vogle.com>
2019-02-03 12:00:02 -0500

    Add licence

--- config/license.java
+++ config/license.java
@@ -16,25 +16,3 @@
  * limitations under the License.
  */
 
-package com.vogle.sbpayment.client;
-
-/**
- * If The transfer data is invalid, to use this
- *
- * @author Allan Im
- */
-public class InvalidAccessException extends Exception {
-
-    private static final long serialVersionUID = 4339467173170618163L;
-
-    /**
-     * Constructs a new InvalidAccessException with the specified detail message.
-     *
-     * @param message the detail message. The detail message is saved for later
-     *                retrieval by the {@link #getMessage()} method.
-     */
-    public InvalidAccessException(String message) {
-        super(message);
-    }
-
-}
