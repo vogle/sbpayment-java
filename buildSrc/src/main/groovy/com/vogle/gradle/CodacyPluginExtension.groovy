70757502f42a3b34f63a08d1bf9bc04fb8272382
Allan Im <allan@vogle.com>
2019-02-12 00:07:05 -0500

    add codacy plugin

---
+++ buildSrc/src/main/groovy/com/vogle/gradle/CodacyPluginExtension.groovy
@@ -0,0 +1,16 @@
+package com.vogle.gradle
+/**
+ * Sets Nexus information
+ *
+ * @author Allan Im
+ *
+ */
+class CodacyPluginExtension {
+
+    // default version
+    String toolVersion = '4.0.5'
+
+    // Jacoco report path
+    Object jacocoReportPath = 'build/reports/jacoco/test/jacocoTestReport.xml'
+
+}
