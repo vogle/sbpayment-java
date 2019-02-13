7401e067a843a5504034b044623bd3eb46eb56db
Allan Im <allan@vogle.com>
2019-02-12 23:19:02 -0500

    Update license

--- sbpayment-creditcard/src/main/java/com/vogle/sbpayment/creditcard/params/BySavedCard.java
+++ sbpayment-creditcard/src/main/java/com/vogle/sbpayment/creditcard/params/BySavedCard.java
@@ -1,7 +1,5 @@
 /*
- * Copyright 2019 VOGLE Labs.
- *
- * This file is part of sbpayment-java - Sbpayment client.
+ * Copyright 2019 Vogle Labs.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
